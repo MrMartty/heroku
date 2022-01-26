18226822e4d5bd8feb4356f9814be42f8c16d0ce
Martin Janda <martinjanda48@gmail.com>
2022-01-26 16:55:47 +0100

    fetch dat

--- public/js/listprepravy.js
+++ public/js/listprepravy.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     //fetch('http://localhost:'+port+'/getAll')
     //fetch(window.location.hostname+':'+port+'/getAll')
-    fetch(window.location.hostname)
+    fetch('listprepravy.html')
     //fetch('/getAll')
     .then(response => response.json())
     //.then(data => loadHTMLTable(data['data']));
