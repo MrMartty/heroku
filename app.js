4b6c3bd6b3270d2f02ecbe66c17a563cb815c155
Martin Janda <martinjanda48@gmail.com>
2022-01-27 23:43:44 +0100

    zmena pripon

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const dbService = require('./dbService');
 const res = require('express/lib/response');
 const port = process.env.PORT || 5000;
 //app.use(favicon(__dirname + '/public/images/favicon.png'));
-app.use(express.static('public', {  extensions: ['html']}));
-
+app.use(express.static('public'));
+//{  extensions: ['html']}
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
@@ -51,7 +51,7 @@ app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname+'/index.html'));
 })*/
 
-app.get('/listprepravy.html', function (request, response) {
+app.get('/prepravy', function (request, response) {
 
     response.sendFile(path.join(__dirname+'/listprepravy.html'));
 })
