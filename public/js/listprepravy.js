f506cb6fd07fe9922f0c54a70236c285c970675e
Martin Janda <martinjanda48@gmail.com>
2022-01-30 16:05:34 +0100

    datum uprava

--- public/js/listprepravy.js
+++ public/js/listprepravy.js
@@ -133,7 +133,7 @@ function loadHTMLTable(data) {
         //tableHtml += `<td>${id}</td>`;
         tableHtml += `<td>${odberatel}</td>`;
         tableHtml += `<td>${cena}</td>`;
-        tableHtml += `<td>${datum.toString('yyyy-MM-dd')}</td>`;
+        tableHtml += `<td>${datum.toLocaleString()}</td>`;
         tableHtml += `<td>${spz}</td>`;
         tableHtml += `<td>${firma_nakl}</td>`;
         tableHtml += `<td>${adresa_nakl}</td>`;
